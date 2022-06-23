db52cc256d0165dec33b0ecfb55433ffaeb3e34a
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-06-23 21:53:05 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-//Navigation Bar
-function iconClick(){
-    location.href="index.html";
+function toggle(){
+    var sec = document.getElementById('sec');
+    var nav = document.getElementById('navigation');
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
 }
-function navbar() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-     }
-}
-
-// Not Allowed
-function NA(){
-  alert('You are currently not allowed! Sorry mate. :(')
-}
-
-
-// FAQ
-$(document).ready(function(){
-  $('#FAQs_container').hide();
-  $('#GoToFAQ').click(function(){
-    $('#main_container').hide();
-    $('#FAQs_container').show();
-  });
-  $('#BackToMain').click(function(){
-    $('#main_container').show();
-    $('#FAQs_container').hide();
-  });
-  $(".flip").click(function(){
-    $(this).next().find(".panel").slideToggle("slow");
-  });
-});
