1dc13ade2af3a761fac3c5a03de9cbf02ed38d81
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-06-27 12:29:57 +0630

    Add files via upload

---
+++ PortfolioOMO/script.js
@@ -0,0 +1,6 @@
+function toggle(){
+    var toggleNav = document.getElementById('toggle');
+    var nav = document.getElementById('navigation');
+    toggleNav.classList.toggle('active');
+    nav.classList.toggle('active');
+}
