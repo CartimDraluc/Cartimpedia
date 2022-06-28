256ff6f70261683ce9c8cef08debc854c147dab3
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-06-28 20:45:29 +0630

    Delete script.js

--- PortfolioTest/script.js
+++
@@ -1,6 +0,0 @@
-function toggle(){
-    var sec = document.getElementById('sec');
-    var nav = document.getElementById('navigation');
-    sec.classList.toggle('active');
-    nav.classList.toggle('active');
-}
