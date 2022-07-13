0a1a8ce5c7eb04aa9b8097953946467e5318bedc
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-07-13 16:34:49 +0630

    v 0.15.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Navigation Bar
+//Top Navigation Bar
 function iconClick(){
     location.href="index.html";
 }
@@ -11,13 +11,7 @@ function navbar() {
      }
 }
 
-// Not Allowed
-function NA(){
-  alert('You are currently not allowed! Sorry mate. :(')
-}
-
-
-// FAQ
+// FAQ Section
 $(document).ready(function(){
   $('#FAQs_container').hide();
   $('#GoToFAQ').click(function(){
@@ -33,6 +27,22 @@ $(document).ready(function(){
   });
 });
 
+/*Dark Mode
+function DarkMode() {
+  var element = document.body;
+  var element2 = document.section;
+  element.classList.toggle("darkmode");
+  element2.classList.toggle("darkmode");
+}*/
+
+
+
+
+// ---- ABOUT US PAGE ----
+// Anchor Tags Not Allowed
+function NA(){
+  alert('You are currently not allowed. Sorry mate.')
+}
 
 // Image Tooltip
 $('text').mousemove(function(e){
