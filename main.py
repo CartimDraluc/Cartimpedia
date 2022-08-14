e9f40e048af432f54233fa6b1be0fd1ebd84c578
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-08-14 10:56:28 +0630

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+print("Hello, I'm Math Bot. How can I help you?")
+sign = input(print("Type +,_,/,*"))
+if sign == "+":
+    first = input(print("type first number"))
+    second = input(print("type second number"))
+    result = int(first) + int(second)
+    print(result)
+
+if sign == "-":
+    first = input(print("type first number"))
+    second = input(print("type second number"))
+    result = int(first) - int(second)
+    print(result)
+
+if sign == "/":
+    first = input(print("type first number"))
+    second = input(print("type second number"))
+    result = int(first) / int(second)
+    print(result)
+
+if sign == "*":
+    first = input(print("type first number"))
+    second = input(print("type second number"))
+    result = int(first) * int(second)
+    print(result)
